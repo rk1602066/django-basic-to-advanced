821ca4411987932be504323751c7cac71d016c4e
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-10-17 02:08:20 -0700

    Create 4views.py

---
+++ blog/4views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse,render
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+    
+def post_list(request):
+  return render(request,'blog/post_list.html',{})
+  
+/*here render({}):{}-> is a python dictionary 
+# we can use like bellow:
+#dict_name{
+"a":1,"b":2
+}
+return render(request,'blog/post_list.html',dict_name)
+*/
