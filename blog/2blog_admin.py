ca66356409ec1974a81c3325a7668a447052709f
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-10-17 01:45:09 -0700

    Update 2blog_admin.py

--- blog/2blog_admin.py
+++ blog/2blog_admin.py
@@ -3,3 +3,7 @@ from .models import Post
  
 admin.site.register(Post)
 //admin panal theke database er post add, edit,delete korte ekhane Post(models-> er classname) k register korte hobe
+
+
+#create super user
+venv: python manage.py createsuperuser
