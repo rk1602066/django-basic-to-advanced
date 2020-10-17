9fac4c9cc6a953711e1043f3892c95c940490184
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-10-17 01:44:13 -0700

    Create 2blog_admin.py

---
+++ blog/2blog_admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Post
+ 
+admin.site.register(Post)
+//admin panal theke database er post add, edit,delete korte ekhane Post(models-> er classname) k register korte hobe
