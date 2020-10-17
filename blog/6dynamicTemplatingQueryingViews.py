4fa4f2a2bb0fa6808cc6206e1652b54bf56e8bbf
rk1602066 <66769695+rk1602066@users.noreply.github.com>
2020-10-17 08:43:57 -0700

    Create 6dynamicTemplatingQueryingViews.py

---
+++ blog/6dynamicTemplatingQueryingViews.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+ 
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
+
+.html:
+
+blog/templates/blog/post_list.html
+
+{{ posts }}
+
+{% for post in posts %}
+    {{ post }}
+{% endfor %}
+
+
+finally:
+
+blog/templates/blog/post_list.html::
+
+<div>
+    <h1><a href="/">My Blog</a></h1>
+</div>
+ 
+{% for post in posts %}
+    <div>
+        <p>published: {{ post.published_date }}</p>
+        <h1><a href="">{{ post.title }}</a></h1>
+        <p>{{ post.text|linebreaksbr }}</p>
+    </div>
+{% endfor %}
